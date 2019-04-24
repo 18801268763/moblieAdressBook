1c980c69a3ba8888cd63ec9942cd08729fb564f0
Oliver <2205456534@qq.com>
2019-04-24 10:10:53 +0800

    使用mint-ui构建基本架构

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <mt-header title="手机通讯录">
+      <mt-button icon="back" slot="left">返回</mt-button>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+
+    <mt-index-list>
+        <mt-index-section v-for="item in items" :index="item.index">
+          <mt-cell v-for="user in item.users" :title="user.name"></mt-cell>
+        </mt-index-section>
+    </mt-index-list>
+  </div>
+</template>
+
+<script>
+  let userData = [
+  {"index":"A", "users": [
+          {"name": "a1","tel": "18801268763"},
+          {"name": "a1","tel": "18801268763"},
+          {"name": "a1","tel": "18801268763"},
+          {"name": "a1","tel": "18801268763"}
+      ]
+    },
+    {"index":"B", "users": [
+          {"name": "a1","tel": "18801268763"},
+          {"name": "a1","tel": "18801268763"},
+          {"name": "a1","tel": "18801268763"},
+          {"name": "a1","tel": "18801268763"}
+      ]
+    },{"index":"C", "users": [
+          {"name": "a1","tel": "18801268763"},
+          {"name": "a1","tel": "18801268763"},
+          {"name": "a1","tel": "18801268763"},
+          {"name": "a1","tel": "18801268763"}
+      ]
+    },{"index":"D", "users": [
+          {"name": "a1","tel": "18801268763"},
+          {"name": "a1","tel": "18801268763"},
+          {"name": "a1","tel": "18801268763"},
+          {"name": "a1","tel": "18801268763"}
+      ]
+    }
+  ];
+export default {
+  name: 'app',
+  data () {
+    return {
+      items: userData
+    }
+  }
+}
+</script>
+
+<style>
+*{
+  margin:0;
+  padding:0;
+}
+
+.mint-indexsection-index{
+  color: red;
+}
+</style>
